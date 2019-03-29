cd43cbe22cc7a5cd8355653861eb8007c77ba62f
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-03-29 00:53:28 +0000

    completed booleans

--- booleans.js
+++ booleans.js
@@ -1,1 +1,34 @@
-// booleans true or false
+// booleans: true or false
+var todosList = {
+    todos: [],
+    displayTodos: function() {
+        console.log('My todos', this.todos);
+    },
+    
+    addTodo: function(todoText) { 
+        this.todos.push({
+            todotext: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    
+    deleteTodo: function(position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
+
+todosList.addTodo('boolean testing');
+//todosList.changeTodo(0, 'second try');
+todosList.toggleCompleted(0);
