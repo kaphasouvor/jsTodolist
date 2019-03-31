67bf5700a6ffb7ca35fc147519e8a9bb903837a2
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-03-31 13:50:52 +0000

    completed js, html and css

--- htmlDOM.js
+++ htmlDOM.js
@@ -1,1 +1,86 @@
 // version07 html and dom
+
+// booleans: true or false
+var todosList = {
+    todos: [],
+    displayTodos: function() {
+        // if statement; checkibng to see if the list is empty
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!');
+        } else {
+            console.log('My todos:');
+            // for (intialization; condition; final-expression)
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('( )', this.todos[i].todoText);
+                }
+            }
+        }
+    },
+
+    addTodo: function(todoText) {
+        this.todos.push({
+            todotext: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        
+        // get number of completed todos.
+        for(var i = 0; i < totalTodos; i++){
+            if(this.todos[i].completed === true){
+                completedTodos++;
+            }
+        }
+        // Case 1: If evertyting is true, make everything false.
+        if (completedTodos === totalTodos){
+            // make everything false
+            for (var i = 0; i < totalTodos; i++){
+                this.todos[i].completed = false;
+            }
+            // Case 1: otherwise, make everything false.
+        }else{
+            for(var i = 0; i < totalTodos; i++){
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }
+};
+
+// 1. We want to get access to the display todos button.
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById("toggleAllButton");
+
+// 2. We want to run the display todos method 
+//    when someone click display todos button.
+displayTodosButton.addEventListener('click', function(){
+    todosList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function(){
+    todosList.toggleAll();
+});
+
